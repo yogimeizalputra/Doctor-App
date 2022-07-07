beba3d44d720bda2c4327b74c9933fbaa87c78a4
yogimp <yogimeizalputra@gmail.com>
2022-07-07 13:07:32 +0700

    Setup Doctor Detail Screen

--- lib/components/top_doctors_list.dart
+++ lib/components/top_doctors_list.dart
@@ -1,6 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:doctor_app/components/top_doctors_card.dart';
 import 'package:doctor_app/models/doctor.dart';
-import 'package:flutter/material.dart';
 
 class TopDoctorsList extends StatelessWidget {
   const TopDoctorsList({Key? key}) : super(key: key);
@@ -12,7 +12,23 @@ class TopDoctorsList extends StatelessWidget {
       itemCount: topDoctors.length,
       itemBuilder: (context, index) {
         return GestureDetector(
-          onTap: () {},
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              '/doctor_details',
+              arguments: Doctor(
+                doctorName: topDoctors[index].doctorName,
+                doctorDescription: topDoctors[index].doctorDescription,
+                doctorHospital: topDoctors[index].doctorHospital,
+                doctorIsOpen: topDoctors[index].doctorIsOpen,
+                doctorNumberOfPatient: topDoctors[index].doctorNumberOfPatient,
+                doctorPicture: topDoctors[index].doctorPicture,
+                doctorRating: topDoctors[index].doctorRating,
+                doctorSpecialty: topDoctors[index].doctorSpecialty,
+                doctorYearOfExperience:
+                    topDoctors[index].doctorYearOfExperience,
+              ),
+            );
+          },
           child: TopDoctorsCard(
             doctor: topDoctors[index],
           ),
